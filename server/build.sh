8a50e4d6af94dcec84d2ea0e810b3ce16f17cc3b
lawl <root@s3cr3t.de>
2012-07-02 20:38:14 +0200

    implement ALL the things

--- server/build.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/sh
-golang-g main.go
-golang-l -o bin/paste main.8 
-rm main.8
-
--- server/main.go
+++ server/main.go
@@ -4,13 +4,19 @@ import (
 	"fmt"
 	"io/ioutil"
 	"net/http"
+	"os"
 	"strconv"
+	"strings"
 )
 
 const COUNTERFILE = "data/count"
 const DATAPATH = "data/"
 const HTTPSTOREPATH = "/store"
 const HTTPGETPATH = "/get/"
+const RESOURCEPATH = "/c/"
+const MAINPATH = "/"
+const UIFILE = "../client/index.html"
+const UIPATH = "../client/"
 const UMASK = 0664
 
 func GetId() int {
@@ -27,6 +33,14 @@ func GetId() int {
 	return id
 }
 
+func FileExists(path string) bool {
+	_, err := os.Stat(path)
+	if err != nil {
+		return false
+	}
+	return true
+}
+
 func Storehandler(w http.ResponseWriter, r *http.Request) {
 	id := GetId()
 	ioutil.WriteFile(DATAPATH+strconv.Itoa(id), []byte(r.FormValue("r")), UMASK)
@@ -37,15 +51,47 @@ func Gethandler(w http.ResponseWriter, r *http.Request) {
 	if _, err := strconv.Atoi(fname); err == nil {
 		c, err := ioutil.ReadFile(DATAPATH + fname)
 		if err == nil {
-			fmt.Fprintf(w, "%s", c)
+			fmt.Fprint(w, c)
 			return
 		}
 	}
-	fmt.Fprintf(w, "%s", "FAIL")
+	fmt.Fprint(w, "FAIL")
+}
+
+func Mainhandler(w http.ResponseWriter, r *http.Request) {
+	ui, err := ioutil.ReadFile(UIFILE)
+	if err != nil {
+		fmt.Fprint(w, "Couldn't read user interface")
+		return
+	}
+	fmt.Fprint(w, string(ui))
+}
+
+func Resourcehandler(w http.ResponseWriter, r *http.Request) {
+	fname := r.URL.Path[len(RESOURCEPATH):]
+    if fname== "" {
+		http.Error(w, "404 File not found", http.StatusNotFound)
+        return;
+    }
+	if strings.Contains(fname, "..") || fname[:1] == "/" {
+		http.Error(w, "Nice try. Now fuck off", http.StatusForbidden)
+		return
+	}
+    fname = UIPATH + fname;
+	if FileExists(fname) {
+		ui, err := ioutil.ReadFile(fname)
+		if err == nil {
+			fmt.Fprint(w, string(ui))
+			return
+		}
+	}
+	http.Error(w, "404 File not found", http.StatusNotFound)
 }
 
 func main() {
 	http.HandleFunc(HTTPSTOREPATH, Storehandler)
 	http.HandleFunc(HTTPGETPATH, Gethandler)
+	http.HandleFunc(RESOURCEPATH, Resourcehandler)
+	http.HandleFunc(MAINPATH, Mainhandler)
 	http.ListenAndServe(":8080", nil)
 }
